bb846c59e05bed3a6e02b893685d844651b7d57c
hdposten <hao.ding@posten.no>
2018-02-23 21:36:12 +0100

    Initial check-in of module xamarindemo4

---
+++ xamarindemo4/xamarindemo4Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace xamarindemo4
+{
+    public partial class xamarindemo4Page : ContentPage
+    {
+        public xamarindemo4Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
